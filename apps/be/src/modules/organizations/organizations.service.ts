6603ac503ece0dfcf396fd2d1d02ff8afe2ac7c3
Edward <e.arechwa@gmail.com>
2025-02-22 23:11:26 +0100

    refactor: add resolvers for organizations

--- apps/be/src/modules/organizations/organizations.service.ts
+++ apps/be/src/modules/organizations/organizations.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
-import { OrganizationRole } from '@prisma/client';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { OrganizationRole, Prisma } from '@prisma/client';
+import { getPaginationData } from 'src/common/utils/pagination';
 import { PrismaService } from '../utils/prisma/prisma.service';
 import { CreateOrganizationInput } from './dto/create-organization.input';
+import { OneOrganizationInput } from './dto/one-organization.input';
+import { OrganizationsListInput } from './dto/organizations-list.input';
 import { GqlOrganization } from './entities/organization.entity';
+import { OrganizationsListPayload } from './payload/organizations-list.payload';
 
 @Injectable()
 export class OrganizationsService {
@@ -48,5 +52,53 @@ export class OrganizationsService {
       owner: organization.owner,
       members: organization._count.members,
     };
+  }
+
+  async list(
+    userId: string,
+    input: OrganizationsListInput,
+  ): Promise<OrganizationsListPayload> {
+    const { limit, page } = input;
+
+    const where: Prisma.OrganizationWhereInput = {
+      members: { some: { id: userId } },
+    };
+
+    const count = await this.prismaService.organization.count({
+      where,
+    });
+
+    const { meta, skip } = getPaginationData(count, page, limit);
+
+    const organizations = await this.prismaService.organization.findMany({
+      include: { owner: true, _count: { select: { members: true } } },
+      where,
+      skip,
+      take: limit,
+    });
+
+    const formattedOrganizations = organizations.map((o) => ({
+      ...o,
+      membersCount: o._count.members,
+    }));
+
+    return {
+      meta,
+      organizations: formattedOrganizations,
+    };
+  }
+
+  async one(
+    userId: string,
+    input: OneOrganizationInput,
+  ): Promise<GqlOrganization> {
+    const organization = await this.prismaService.organization.findUnique({
+      where: { id: input.organizationId, members: { some: { id: userId } } },
+      include: { _count: { select: { members: true } }, owner: true },
+    });
+
+    if (!organization) throw new NotFoundException('Organization not found');
+
+    return { ...organization, membersCount: organization._count.members };
   }
 }
